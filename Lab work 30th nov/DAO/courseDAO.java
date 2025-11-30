0a2f22389b2a2e737a44c6fec7cd0f9111de4a42
sneha-tech20 <sneha.y722765@gmail.com>
2025-11-30 22:17:17 +0530

    Update courseDAO.java

--- Lab work 30th nov/DAO/courseDAO.java
+++ Lab work 30th nov/DAO/courseDAO.java
@@ -1,1 +1,21 @@
+//package com.knmodi.collage.dao;
+//
+//public interface courseDAO {
+	package com.knmodi.collage.dao;
+	import com.knmodi.collage.model.course;
+	import java.util.ArrayList;
+	public interface courseDAO 
+	{
+		//method for inserting new course data
+		int save(course courseData);
+		
+		//method to fetch all course list
+		ArrayList<course> findAll();
+		
+		//method to fetch a particular course
+		course findByCourseId(String courseId);
+		
+	}
+
+
 
