49a3a0dd1420f29c0c573c8a6416358da4ad1c68
sneha-tech20 <sneha.y722765@gmail.com>
2025-11-30 22:21:52 +0530

    Update facultyDAO.java

--- Lab work 30th nov/DAO/facultyDAO.java
+++ Lab work 30th nov/DAO/facultyDAO.java
@@ -1,1 +1,19 @@
+//package com.knmodi.collage.dao;
+//
+//public interface facultyDAO {
+	package com.knmodi.collage.dao;
+	import com.knmodi.collage.model.faculty;
+	import java.util.ArrayList;
+	public interface facultyDAO 
+	{
+		//method for inserting new faculty data
+		int save(faculty courseData);
+		
+		//method to fetch all faculty list
+		ArrayList<faculty> findAll();
+		
+		//method to fetch a particular faculty
+		faculty findByfacultyId(String facultyId);
+		
+	}
 
