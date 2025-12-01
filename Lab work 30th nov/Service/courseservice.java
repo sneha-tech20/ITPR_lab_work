fdce276fab905b3e6a24298e97e3e04d49dce8fb
sneha-tech20 <sneha.y722765@gmail.com>
2025-12-01 14:51:28 +0530

    Update courseservice.java

--- Lab work 30th nov/Service/courseservice.java
+++ Lab work 30th nov/Service/courseservice.java
@@ -1,1 +1,15 @@
-
+package com.knmodi.collage.service;
+import  com.knmodi.collage.model.*;
+public interface CourseService 
+{
+	//method to register new course
+	void registerCourse(course course);
+	//method to update course duration
+	void updateCourseDuration(String courseId,int duration);
+	//method to fetch all courses
+	void getCourseList();
+	//method to fetch course details of a particular course
+	void getCourseDetails(String courseId);
+	//
+	
+}
